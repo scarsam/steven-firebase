4091e697d220bf0ed3fc8c01e300f1d699f493e4
Sam Ojling <sam.ojling@zenpayroll.com>
2019-08-16 16:00:37 -0700

    Started adding 'fake' data

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 import CopyClipboard from "./CopyClipboard";
 import { connect } from "react-redux";
 import { getGroup } from "../store/actions/groupAction";
+import Box from "./styles/Box";
+
+const GroupStyles = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TopBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  input {
+    position: absolute;
+  }
+`;
+
+const BorderBottom = styled.div`
+  border-bottom: 1px solid black;
+`;
+
+const Wrapper = styled.div`
+  align-items: center;
+  justify-content: space-between;
+  display: flex;
+`;
 
 function Group(props) {
   const { getGroup, group } = props;
@@ -14,8 +40,32 @@ function Group(props) {
   return (
     group && (
       <>
+        <Box>
+          <TopBar>
+            <p>Group name: {group.name}</p>
+          </TopBar>
+          <Wrapper>
+            <p>You owe -$150</p>
+            <button>Get even</button>
+          </Wrapper>
+          <BorderBottom />
+          <GroupStyles>
+            <p>Sam Ojling</p>
+            <p>Wine</p>
+            <p>-$100</p>
+          </GroupStyles>
+          <GroupStyles>
+            <p>Max Moubabe</p>
+            <p>Taxi</p>
+            <p>+$100</p>
+          </GroupStyles>
+          <GroupStyles>
+            <p>King Gabbe</p>
+            <p>Water</p>
+            <p>-$150</p>
+          </GroupStyles>
+        </Box>
         <CopyClipboard />
-        <p>Group name: {group.name}</p>
       </>
     )
   );
