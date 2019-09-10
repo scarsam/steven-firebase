722f97285199df9d3bb79d63f13b1525194ccc6a
Sam Ojling <sam.ojling@zenpayroll.com>
2019-09-10 11:07:41 -0700

    Implemented the SO Expense solution

--- src/components/Group/GetEven.js
+++ src/components/Group/GetEven.js
@@ -9,6 +9,41 @@ function GetEven({ groupId, group }) {
   const dispatch = useDispatch();
   const totalExpenses = useSelector(store => store.expenseState.totalExpenses);
 
+  const splitPayments = payments => {
+    const people = Object.keys(payments);
+    const valuesPaid = Object.values(payments);
+
+    const sum = valuesPaid.reduce((acc, curr) => curr + acc);
+    const mean = sum / people.length;
+
+    const sortedPeople = people.sort(
+      (personA, personB) => payments[personA] - payments[personB]
+    );
+    const sortedValuesPaid = sortedPeople.map(
+      person => payments[person] - mean
+    );
+
+    let i = 0;
+    let j = sortedPeople.length - 1;
+    let debt;
+
+    while (i < j) {
+      debt = Math.min(-sortedValuesPaid[i], sortedValuesPaid[j]);
+      sortedValuesPaid[i] += debt;
+      sortedValuesPaid[j] -= debt;
+
+      console.log(`${sortedPeople[i]} owes ${sortedPeople[j]} $${debt}`);
+
+      if (sortedValuesPaid[i] === 0) {
+        i++;
+      }
+
+      if (sortedValuesPaid[j] === 0) {
+        j--;
+      }
+    }
+  };
+
   useEffect(() => {
     dispatch(fetchAllExpenses(groupId, group.users));
   }, [groupId, group.users, dispatch]);
@@ -20,13 +55,7 @@ function GetEven({ groupId, group }) {
       </Modal.Header>
       <Modal.Body>
         <Container>
-          {totalExpenses.map(expense => (
-            <Row>
-              <p>
-                {expense.name} - {expense.total}
-              </p>
-            </Row>
-          ))}
+          <Row>{splitPayments(totalExpenses)}</Row>
         </Container>
       </Modal.Body>
     </>
